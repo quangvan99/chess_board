7372ed97612ccfe8ad7718e7b3139df8451c5c7f
duypham2801 <duyphamxv2801@gmail.com>
2024-12-18 04:38:43 +0000

    rm dis

--- det/main.py
+++ det/main.py
@@ -2,7 +2,7 @@ from base import BasePipeline
 from cfg import source, detection_tracking, viz, diplaysink, filesink, segment
 
 if __name__ == "__main__":
-    str_pipeline = {**source, **detection_tracking, **viz, **diplaysink}
+    str_pipeline = {**source, **detection_tracking, **viz, **filesink}
     # print(str_pipeline)
     pipeline = BasePipeline()
     pipeline.run(str_pipeline)
